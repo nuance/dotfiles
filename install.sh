4e28b3f17e806e9ea73a3a8963a752705172d276
Matt Jones <matt@mhjones.org>
2014-01-03 12:11:09 -0800

    dotfiles initial import

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+mkdir ~/.backup.d
+for file in `ls`; do
+    if [ "$file" == "install.sh" ] || [ "$file" == "README.md" ] || [ -d "$file" ]; then
+        continue
+    fi
+
+    if [[ -e ~/.$file ]]; then
+        echo "Backing up .$file"
+        mv ~/.$file ~/.backup.d/
+    fi
+
+    ln -s `pwd`/$file ~/.$file
+done
+
+for dir in `ls`; do
+    if [ ! -d "$dir" ]; then
+        continue
+    fi
+
+    mkdir ~/.$dir
+    for file in `ls $dir`; do
+        if [[ -e ~/.$dir/$file ]]; then
+            echo "Backing up .$dir/$file"
+            mv ~/.$dir/$file ~/.backup.d/
+        fi
+
+        ln -s `pwd`/$dir/$file ~/.$dir/$file
+    done
+done
