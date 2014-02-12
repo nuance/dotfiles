c4130f8d4f0cf031f3e4a16226cd2e36622263d8
Matt Jones <matt@mhjones.org>
2014-02-12 10:42:06 -0800

    Add ssh / sp / s3cmd completion, add virtualenv sourcer & completer

--- bash_includes/completions.sh
+++ bash_includes/completions.sh
@@ -1,1 +1,5 @@
-complete -W "$(echo $(grep '^ssh ' .bash_history | sort -u | sed 's/^ssh //'))" ssh
+complete -W "$(echo $(egrep '^(ssh|sp) ' .bash_history | sort -u | sed 's/^ssh //' | sed 's/^sp //'))" ssh
+complete -W "$(echo $(egrep '^(ssh|sp) ' .bash_history | sort -u | sed 's/^ssh //' | sed 's/^sp //'))" sp
+complete -W "$(echo $(egrep '^s3cmd ' .bash_history | sort -u | sed 's/^s3cmd //'))" s3cmd
+
+complete -W "$(echo $(ls -d ~/*.virt | cut -f4 -d'/' | sed 's/.virt//'))" v
