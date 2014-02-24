4c819539d5056268ae3b101edd2496ab91c60c1a
Matthew Jones <matt@mhjones.org>
2014-02-23 19:46:49 -0800

    add more supported commands

--- bash_includes/go_complete.sh
+++ bash_includes/go_complete.sh
@@ -5,7 +5,7 @@ _go()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-    if [[ ${prev} == "get" ]] && [[ -d src ]]; then
+    if [[ "get install build test" =~ ${prev} ]] && [[ -d src ]]; then
         COMPREPLY=( $(compgen -W "$(find src/* -type d | sed 's/src\///')" -- ${cur}))
         return 0
     fi
