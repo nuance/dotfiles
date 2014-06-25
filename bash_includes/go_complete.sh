10e31baa98a0b099d4f83d7926fb0e31721f1753
Matt Jones <matt@mhjones.org>
2014-06-25 11:23:31 -0700

    fix -v handling

--- bash_includes/go_complete.sh
+++ bash_includes/go_complete.sh
@@ -5,6 +5,10 @@ _go()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
 
+    if [[ ${prev} == "-v" ]]; then 
+        prev="${COMP_WORDS[COMP_CWORD-2]}"
+    fi
+
     if [[ "get install build test" =~ ${prev} ]] && [[ -d src ]]; then
         COMPREPLY=( $(compgen -W "$(find src/* -type d | sed 's/src\///')" -- ${cur}))
         return 0
