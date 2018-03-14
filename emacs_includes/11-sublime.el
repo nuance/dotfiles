bdcc5f5337d0b629c8c248df17016573a065c869
Matt Jones <matt@mhjones.org>
2018-03-14 13:33:31 -0700

    More sublime

--- emacs_includes/11-sublime.el
+++ emacs_includes/11-sublime.el
@@ -3,19 +3,19 @@
 (require 'multiple-cursors)
 (global-set-key (kbd "s-L") 'mc/edit-lines)
 
-(defun select-symbol-or-mark-all ()
+(defun select-symbol ()
     "Sets the region to the symbol under the point"
     (interactive)
-    (if (use-region-p)
-	(progn
-	  (mc/mark-all-dwim nil))
-      (let ((bounds (find-tag-default-bounds)))
-	(progn
-	  (push-mark (cdr bounds))
-	  (goto-char (car bounds))
-	  (setq mark-active t)))))
+    (let ((bounds (find-tag-default-bounds)))
+      (progn
+	(set-mark (car bounds))
+	(goto-char (cdr bounds))
+	(setq mark-active t))))
 
-(global-set-key (kbd "s-d") 'select-symbol-or-mark-all)
+(global-set-key (kbd "s-d") 'select-symbol)
+(global-set-key (kbd "s-D") 'mc/mark-all-symbols-like-this)
+(global-set-key (kbd "s-<mouse-1>") 'mc/add-cursor-on-click)
+
 (global-set-key (kbd "C-\\") 'comment-or-uncomment-region)
 (global-set-key (kbd "C-s") 'isearch-forward-regexp)
 (global-set-key (kbd "s-P") 'smex)
