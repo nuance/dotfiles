f50f850007af476c6b12841092ab1e835a996c08
Matthew Jones <matt@mhjones.org>
2018-03-15 16:37:35 -0700

    sublime-style comment handling

--- emacs_includes/11-sublime.el
+++ emacs_includes/11-sublime.el
@@ -16,7 +16,16 @@
 (global-set-key (kbd "s-D") 'mc/mark-all-symbols-like-this)
 (global-set-key (kbd "s-<mouse-1>") 'mc/add-cursor-on-click)
 
-(global-set-key (kbd "C-\\") 'comment-or-uncomment-region)
+(defun comment-line-or-region (beg end)
+  "Comment a region or the current line."
+  (interactive "*r")
+  (save-excursion
+    (if (region-active-p)
+	(comment-or-uncomment-region beg end)
+      (comment-line 1))))
+
+(global-set-key (kbd "C-\\") 'comment-line-or-region)
+(global-set-key (kbd "s-/") 'comment-line-or-region)
 (global-set-key (kbd "C-s") 'isearch-forward-regexp)
 (global-set-key (kbd "s-P") 'smex)
 
