a350066d10b4b826815cf0053cabea31cbc84196
Matt Jones <matt@mhjones.org>
2018-03-21 11:38:36 -0700

    simplify

---
+++ emacs_includes/00-appearance.el
@@ -0,0 +1,22 @@
+;; Prevent the cursor from blinking
+(blink-cursor-mode 0)
+;; Don't use messages that you don't read
+(setq initial-scratch-message "")
+(setq inhibit-startup-message t)
+;; Don't let Emacs hurt your ears
+(setq visible-bell t)
+
+(setq ring-bell-function
+      (lambda ()
+        (let ((orig-fg (face-foreground 'mode-line)))
+          (set-face-foreground 'mode-line "#F2804F")
+          (run-with-idle-timer 0.1 nil
+                               (lambda (fg) (set-face-foreground 'mode-line fg))
+                               orig-fg))))
+
+(setq inhibit-startup-echo-area-message "matt")
+
+(if (boundp 'toggle-frame-fullscreen) (toggle-frame-fullscreen))
+(if (boundp 'scroll-bar-mode) (scroll-bar-mode 0))
+(if (boundp 'tool-bar-mode) (tool-bar-mode 0))
+(if (boundp 'menu-bar-mode) (menu-bar-mode 0))
--- emacs_includes/10-company.el
+++ emacs_includes/10-company.el
@@ -1,4 +1,5 @@
 (add-hook 'after-init-hook 'global-company-mode)
 (setq company-idle-delay 0.1)
 (company-quickhelp-mode)
+(setq company-quickhelp-delay 0.1)
 
