ddb3fa1094d36f65246dde143e1e7e0288d6fd4f
Matthew Jones <matt@mhjones.org>
2018-03-22 13:22:25 -0700

    python snippets and company mode tweaks

--- emacs_includes/08-python.el
+++ emacs_includes/08-python.el
@@ -35,4 +35,9 @@
     ;; install jedi-server on first run
     (unless (file-directory-p "~/.emacs.d/.python-environments/default")
       (jedi:install-server-block))
-    (add-to-list 'company-backends 'company-jedi)))
+    (add-to-list 'company-backends '(company-jedi company-yasnippet))))
+
+(use-package yasnippet
+  :straight t
+  :init (setq yas-snippet-dirs '("~/.emacs_includes/snippets"))
+  :config (yas-global-mode 1))
