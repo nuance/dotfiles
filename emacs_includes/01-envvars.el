726e1e4baafc3a59588770490c2eb0914d70ec22
Matt Jones <matt@mhjones.org>
2018-03-28 22:18:29 -0700

    Load env vars from shell, even in windowed (app) mode

--- emacs_includes/01-envvars.el
+++ emacs_includes/01-envvars.el
@@ -1,2 +1,4 @@
-(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
-
+(when window-system
+  (let ((path-from-shell (shell-command-to-string "/bin/bash -l -c 'echo $PATH'")))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
