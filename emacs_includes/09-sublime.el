407208a2d651cc71fcfd9b5c18fcd10150bef220
Matthew Jones <matt@mhjones.org>
2018-07-03 11:44:50 -0700

    add expand-region, dedent / indent

--- emacs_includes/09-sublime.el
+++ emacs_includes/09-sublime.el
@@ -2,22 +2,22 @@
 
 (setq require-final-newline t)
 
+(use-package expand-region
+  :ensure t
+  :bind (("s-f" . 'er/expand-region)
+         ("s-F" . 'er/contract-region)))
+
 (use-package multiple-cursors
   :ensure t
   :config
   (defun select-symbol (arg)
     "Sets the region to the symbol under the point"
     (interactive "p")
-    (if mark-active (mc/mark-next-like-this arg)
-      (let ((bounds (find-tag-default-bounds)))
-        (progn
-          (set-mark (car bounds))
-          (goto-char (cdr bounds))
-          (setq mark-active t)))))
+    (if mark-active (mc/mark-next-like-this arg) (er/mark-symbol)))
   (defun mark-all-like-symbol (arg)
     (interactive "p")
     (progn
-      (unless mark-active (select-symbol arg))
+      (unless mark-active (er/mark-symbol))
       (mc/mark-all-like-this)))
   (add-to-list 'mc/unsupported-minor-modes 'company-mode)
   (add-to-list 'mc/unsupported-minor-modes 'company-quickhelp-mode)
@@ -29,6 +29,7 @@
   (add-to-list 'mc/unsupported-minor-modes 'lsp-ui-mode)
   :bind (("s-L" . 'mc/edit-lines)
          ("s-d" . 'select-symbol)
+         ("s-M-d" . 'er/mark-symbol)
          ("s-D" . 'mark-all-like-symbol)
          ("s-<mouse-1>" . 'mc/add-cursor-on-click)))
 
@@ -37,8 +38,10 @@
   (interactive "*r")
   (save-excursion
     (if (region-active-p)
-	(comment-or-uncomment-region beg end)
+        (comment-or-uncomment-region beg end)
       (comment-line 1))))
+
+(global-set-key (kbd "s-a") 'mark-whole-buffer)
 
 (global-set-key (kbd "C-\\") 'comment-line-or-region)
 (global-set-key (kbd "s-/") 'comment-line-or-region)
@@ -54,6 +57,17 @@
 
 (global-set-key (kbd "s-l") 'goto-line)
 
+(defun dedent (start end)
+  (interactive "*r")
+  (indent-rigidly start end (- tab-width)))
+
+(defun indent (start end)
+  (interactive "*r")
+  (indent-rigidly start end tab-width))
+
+(global-set-key (kbd "s-[") 'dedent)
+(global-set-key (kbd "s-]") 'indent)
+
 (use-package dtrt-indent
   :ensure t
   :config
@@ -63,11 +77,14 @@
 (electric-pair-mode 1)
 (show-paren-mode)
 
-(use-package nlinum
-  :ensure t
-  :quelpa (nlinum-1.8.1 :fetcher file :path "~/.emacs_includes/external/nlinum-1.8.1.el")
-  :config
-  (global-nlinum-mode))
+(progn
+  (setq quelpa-stable-p nil)
+  (use-package nlinum
+    :ensure t
+    ;; :quelpa (nlinum-1.8.1 :fetcher file :path "~/.emacs_includes/external/nlinum-1.8.1.el")
+    :config
+    (global-nlinum-mode))
+  (setq quelpa-stable-p t))
 
 (when (window-system)
   (use-package diff-hl
