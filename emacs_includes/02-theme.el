bb26c8772d650ca9b69411a2d37bb765fbdd26a0
Matt Jones <matt@mhjones.org>
2018-12-20 13:33:12 -0500

    switch to just gotham

--- emacs_includes/02-theme.el
+++ emacs_includes/02-theme.el
@@ -1,10 +1,5 @@
-(use-package gruvbox-theme
-  :straight t
-  :config
-  (load-theme 'gruvbox-dark-medium t))
-
 (use-package gotham-theme
   :straight t
   :config
-  (set 'gotham-tty-256-colors t)
+  (customize-set-variable 'gotham-tty-256-colors t)
   (load-theme 'gotham t))
