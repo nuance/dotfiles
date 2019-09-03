e283c6b72ce8139be6b0d287f91fa9ad7b42eb75
Matthew Jones <matt@mhjones.org>
2019-09-03 14:16:08 -0700

    usage tweaks

--- emacs_includes/16-python.el
+++ emacs_includes/16-python.el
@@ -5,13 +5,13 @@
     :hook (python-mode . blacken-mode))
   (setq quelpa-stable-p t))
 
-(use-package company-jedi
-  :straight t
-  :config
-  ;; install jedi-server on first run
-  (unless (file-directory-p "~/.emacs.d/.python-environments/default")
-    (jedi:install-server-block))
-  (add-to-list 'company-backends '(company-jedi company-yasnippet)))
+;; (use-package company-jedi
+;;   :straight t
+;;   :config
+;;   ;; install jedi-server on first run
+;;   (unless (file-directory-p "~/.emacs.d/.python-environments/default")
+;;     (jedi:install-server-block))
+;;   (add-to-list 'company-backends '(company-jedi company-yasnippet)))
 
 (use-package yasnippet
   :straight t
