281a569282eb15d4bc3097c9ed588a0d4d57d46d
Matthew Jones <matt@mhjones.org>
2019-09-30 16:54:38 -0400

    remove the old config

--- emacs_includes/01-envvars.el
+++
@@ -1,4 +0,0 @@
-(when window-system
-  (let ((path-from-shell (shell-command-to-string "/bin/bash -l -c 'echo $PATH'")))
-    (setenv "PATH" path-from-shell)
-    (setq exec-path (split-string path-from-shell path-separator))))
--- emacs_includes/02-theme.el
+++
@@ -1,5 +0,0 @@
-(use-package gotham-theme
-  :straight t
-  :config
-  (customize-set-variable 'gotham-tty-256-colors t)
-  (load-theme 'gotham t))
