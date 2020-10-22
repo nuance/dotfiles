d81c75d7d8870eec0a6edea285c40b543b11d2f8
Matthew Jones <matt@mhjones.org>
2020-10-22 11:08:21 -0400

    Update frozen emacs version / packages

--- nix/pro.nix
+++ nix/pro.nix
@@ -8,7 +8,7 @@ in
     (
       import (
         builtins.fetchTarball {
-          url = https://github.com/nix-community/emacs-overlay/archive/924f61bfcaa53bbba502635dd23ffe823e79fb45.tar.gz;
+          url = https://github.com/nix-community/emacs-overlay/archive/5cb7caf09e90749dd394152eb8704265979a7470.tar.gz;
         }
       )
     )
