57ca61927353e097105c33e294700bd52f0c8594
Matthew Jones <matt@mhjones.org>
2020-12-11 15:58:17 -0500

    Remove a bunch of unused config files

--- bash_includes/nix.sh
+++
@@ -1,1 +0,0 @@
-export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
--- setup_osx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# install default packages
-brew install $(cat brew_packages)
