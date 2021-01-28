bf63b738d8a76345de88b4ae7899e87656fbab92
Matthew Jones <matt@mhjones.org>
2021-01-28 09:28:01 -0500

    manually set tmux prefix

--- nix/programs/tmux.nix
+++ nix/programs/tmux.nix
@@ -3,7 +3,13 @@
   programs.tmux = {
     enable = true;
     terminal = "screen-256color";
-    extraConfig = "setw -g mouse on";
-    prefix = "C-o";
+    extraConfig = ''
+      setw -g mouse on
+
+      # rebind main key: C-o
+      unbind C-b
+      set -g prefix C-o
+      bind C-o send-prefix
+    '';
   };
 }
