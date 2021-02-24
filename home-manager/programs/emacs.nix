214b3343ba42bc1101795e2b6b920279b59b451f
Matthew Jones <matt@mhjones.org>
2021-02-24 15:42:42 -0500

    Drop straight.el in favor of nix

--- home-manager/programs/emacs.nix
+++ home-manager/programs/emacs.nix
@@ -1,28 +1,26 @@
 { pkgs, lib, ... }:
 let
-  emacs = pkgs.emacsGcc;
+  emacs = (pkgs.emacsWithPackagesFromUsePackage {
+    config = ./emacs/init.org;
+    alwaysEnsure = true;
+    alwaysTangle = true;
+    package = pkgs.emacsGcc;
+  });
 in
 {
-  home.packages = with pkgs; [
-    emacs
-    gnupg1
-    ispell
-    nixpkgs-fmt
-    python37Packages.black
-    python37Packages.flake8
-    emacs-all-the-icons-fonts
-  ];
+  home.packages = with pkgs;
+    [
+      emacs
+      gnupg1
+      ispell
+      nixpkgs-fmt
+      python37Packages.black
+      python37Packages.flake8
+      emacs-all-the-icons-fonts
+    ];
 
   home.file = {
     "Applications/Emacs.app".source = "${emacs}/Applications/Emacs.app";
-
-    ".emacs.d/init.org" = {
-      source = ./emacs/init.org;
-      onChange = "cd ~/.emacs.d ; ${emacs}/bin/emacs --batch -l ob-tangle --eval \"(org-babel-tangle-file \\\"init.org\\\")\" ; ${emacs}/bin/emacs --batch --load init.el --eval \"(straight-thaw-versions)\";";
-    };
-    ".emacs.d/straight/versions/default.el" = {
-      source = ./emacs/straight-package-versions.el;
-      onChange = "cd ~/.emacs.d ; ${emacs}/bin/emacs --batch -l ob-tangle --eval \"(org-babel-tangle-file \\\"init.org\\\")\" ; ${emacs}/bin/emacs --batch --load init.el --eval \"(straight-thaw-versions)\";";
-    };
+    ".emacs.d/init.org".source = ./emacs/init.org;
   };
 }
