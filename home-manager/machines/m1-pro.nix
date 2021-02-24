56c4443d5227a927745b7c1a6bc9760f7d2cb851
Matthew Jones <matt@mhjones.org>
2021-02-24 14:55:43 -0500

    pull crypted files out of ci build

--- home-manager/machines/m1-pro.nix
+++ home-manager/machines/m1-pro.nix
@@ -1,6 +1,6 @@
 { ... }:
 {
-  imports = [ ../config.nix ../environments/macos.nix ];
+  imports = [ ../config.nix ../environments/macos.nix ../programs/emacs-server.nix ];
 
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
