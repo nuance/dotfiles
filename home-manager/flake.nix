e4e5c324fb2c9532e318c24630177c6d0fe60ddf
Matthew Jones <matt@mhjones.org>
2021-02-24 14:24:54 -0500

    Use a github-specific config to deal with the username / homedir differing

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -43,6 +43,28 @@
           username = "matt";
         };
 
+        github-macos-ci = inputs.home-manager.lib.homeManagerConfiguration {
+          configuration = { pkgs, config, ... }:
+            {
+              xdg.configFile."nix/nix.conf".text = ''
+                experimental-features = nix-command flakes
+                substituters = https://cache.nixos.org https://nuance.cachix.org
+                trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nuance.cachix.org-1:dAmExyWto63NWNdWaXvVLwmwewO+e/bXs4uAv9uf1No=
+              '';
+              nixpkgs.overlays = overlays;
+              imports = [
+                ./machines/github-macos-ci.nix
+              ];
+
+              programs.bash.shellAliases.flake-rebuild = "(cd ~/dotfiles/home-manager && ${./rebuild.sh} m1-pro)";
+            };
+
+          system = "x86_64-darwin";
+
+          homeDirectory = "/Users/runner";
+          username = "runner";
+        };
+
         air = inputs.home-manager.lib.homeManagerConfiguration {
           configuration = { pkgs, config, ... }:
             {
@@ -82,6 +104,7 @@
       };
 
       m1-pro = self.homeConfigurations.m1-pro.activationPackage;
+      github-macos-ci = self.homeConfigurations.github-macos-ci.activationPackage;
       air = self.homeConfigurations.air.activationPackage;
       dl = self.homeConfigurations.dl.activationPackage;
     };
