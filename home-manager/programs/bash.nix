c73982513876b96ebfd83f6525493cf292bc619c
Matthew Jones <matt@mhjones.org>
2021-02-25 15:06:21 -0500

    Simplify bash config a bit

--- home-manager/programs/bash.nix
+++ home-manager/programs/bash.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.bash = {
     enable = true;
@@ -11,31 +11,10 @@
     };
 
     initExtra = ''
-      if [[ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]]; then
-          # shellcheck source=/dev/null
-          . "$HOME/.nix-profile/etc/profile.d/nix.sh"
-      fi
-
-      if [[ -e $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh ]]; then
-          # shellcheck source=/dev/null
-          . $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
-          export NIX_PATH=$HOME/.nix-defexpr/channels''${NIX_PATH:+:}$NIX_PATH
-      fi
-
-      e() {
-          $EDITOR "$@"
-      }
-
-      if [[ -d ~/.bash_includes ]]; then
-          for src in ~/.bash_includes/*; do
-              . "$src"
-          done
-      fi
-
       # PS1 is "(remote host name)? (directory) ([non-zero exit code])? $"
       export PS1="\[\033[0;32m\]\W\[\033[00m\] \$(exit_code="\$?"; ((\$exit_code)) && echo \"\[\033[0;31m\][\$exit_code] $\" || echo \"\[\033[0;32m\]$\" )\[\033[00m\] "
       if [[ "$(uname)" != Darwin ]]; then
-          export PS1="\[\033[0;33m\]\h\[\033[00m\] $PS1"
+        export PS1="\[\033[0;33m\]\h\[\033[00m\] $PS1"
       fi
     '';
 
@@ -43,6 +22,7 @@
       g = "git";
       gg = "git grep";
       onepage = "head -n $(echo \"$(tput lines) - 2\" | bc)";
+      e = "$EDITOR";
     };
 
     shellOptions = [
