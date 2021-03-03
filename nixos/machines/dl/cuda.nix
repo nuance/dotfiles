987f4efd3f862ae2f050d00f93b5a93e52b34ce8
Matthew Jones <matt@mhjones.org>
2021-03-03 09:36:30 -0500

    split out cuda config, don't run it during ci

--- nixos/machines/dl/cuda.nix
+++ nixos/machines/dl/cuda.nix
@@ -1,10 +1,5 @@
 { pkgs, ... }:
 {
-  hardware.opengl.enable = true;
-  services.xserver.videoDrivers = [ "nvidia" ];
-
-  hardware.nvidia.nvidiaPersistenced = true;
-
   environment.systemPackages = with pkgs; [
     cudatoolkit_11
   ];
