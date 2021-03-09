aa4e8fe21fdd150ecd33e5337ac5b15e8b138004
Matthew Jones <matt@mhjones.org>
2021-03-09 10:11:55 -0500

    Add work laptop configuration

---
+++ home-manager/machines/work-pro.nix
@@ -0,0 +1,44 @@
+{ overlays, ... }:
+{
+
+  system = "x86_64-darwin";
+
+  homeDirectory = "/Users/matt";
+  username = "matt";
+
+  configuration = { pkgs, lib, config, ... }:
+    {
+      nixpkgs.overlays = overlays;
+      programs.bash.shellAliases.flake-rebuild = "(cd ~/dotfiles/home-manager && ${../rebuild.sh} work-pro)";
+
+      imports = [
+        ../config.nix
+        ../environments/macos.nix
+        ../environments/terminal.nix
+        ../programs/emacs-server.nix
+        ./secrets/work-pro.nix
+      ];
+
+      programs.bash.sessionVariables = {
+        NVM_DIR = "/Users/matt/.nvm";
+      };
+
+      programs.bash.initExtra = ''
+        . "/Users/matt/.nvm/nvm.sh"
+        . "/Users/matt/.nvm/bash_completion"
+      '';
+
+      programs.ssh.serverAliveCountMax = 1;
+      programs.ssh.serverAliveInterval = 240;
+
+      # This value determines the Home Manager release that your
+      # configuration is compatible with. This helps avoid breakage
+      # when a new Home Manager release introduces backwards
+      # incompatible changes.
+      #
+      # You can update Home Manager without changing this value. See
+      # the Home Manager release notes for a list of state version
+      # changes in each release.
+      home.stateVersion = "21.03";
+    };
+}
