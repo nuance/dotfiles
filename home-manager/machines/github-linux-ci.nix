8c575616323b1fcab08d7ab848fae420237e16ba
Matthew Jones <matt@mhjones.org>
2021-03-09 10:05:28 -0500

    Simplify flake.nix

--- home-manager/machines/github-linux-ci.nix
+++ home-manager/machines/github-linux-ci.nix
@@ -1,19 +1,25 @@
-{ ... }:
+{ overlays, ... }:
 {
-  imports = [ ../config.nix ];
 
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = "runner";
-  home.homeDirectory = "/home/runner";
+  system = "x86_64-linux";
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "21.03";
+  homeDirectory = "/home/runner";
+  username = "runner";
+
+  configuration = { pkgs, lib, config, ... }:
+    {
+      nixpkgs.overlays = overlays;
+      programs.bash.shellAliases.flake-rebuild = "(cd ~/dotfiles/home-manager && ${../rebuild.sh} github-linux-ci)";
+      imports = [ ../config.nix ];
+
+      # This value determines the Home Manager release that your
+      # configuration is compatible with. This helps avoid breakage
+      # when a new Home Manager release introduces backwards
+      # incompatible changes.
+      #
+      # You can update Home Manager without changing this value. See
+      # the Home Manager release notes for a list of state version
+      # changes in each release.
+      home.stateVersion = "21.03";
+    };
 }
