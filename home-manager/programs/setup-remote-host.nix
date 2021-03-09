7713e5863f6d1e7855e2e3b1f0eb834d2924c51e
Matthew Jones <matt@mhjones.org>
2021-03-09 09:08:47 -0500

    Use correct interpreter

--- home-manager/programs/setup-remote-host.nix
+++ home-manager/programs/setup-remote-host.nix
@@ -4,10 +4,10 @@
     function setup-remote-host () {
       hostname=$1; shift;
 
-      scp ${./setup-remote-host/remote-profile} $hostname:.remote-profile
-      scp ${./setup-remote-host/secrets/remote-editor} $hostname:.remote-editor
-      scp ${./emacs/secrets/server} $hostname:.remote-emacs-server
-      ssh $hostname "echo -n 'Adding .remote-profile to .bash_profile... ' ; [[ \$(grep REMOTE-EDITOR ~/.bash_profile) ]] || echo '# start REMOTE-EDITOR\n[[-f ~/.remote-profile]] && . ~/.remote-profile\n# end REMOTE-EDITOR\n' >> .bash_profile && echo ok"
+      scp -q ${./setup-remote-host/remote-profile} $hostname:.remote-profile
+      scp -q ${./setup-remote-host/secrets/remote-editor} $hostname:.remote-editor
+      scp -q ${./emacs/secrets/server} $hostname:.remote-emacs-server
+      ssh $hostname "echo -n 'Adding .remote-profile to .bash_profile... ' ; [[ \$(grep REMOTE-EDITOR ~/.bash_profile) ]] || echo \"# start REMOTE-EDITOR\n[[-f ~/.remote-profile]] && . ~/.remote-profile\n# end REMOTE-EDITOR\n\" >> .bash_profile && echo ok"
     }
   '';
 }
