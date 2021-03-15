6b1f027327e32c1299d6b303bf3ce54e5a847b44
Matthew Jones <matt@mhjones.org>
2021-03-15 08:47:45 -0400

    Snapshot org files

--- home-manager/environments/macos-launch-agents.nix
+++ home-manager/environments/macos-launch-agents.nix
@@ -30,6 +30,29 @@ in
     onChange = "/bin/launchctl stop org.mhjones.ical-to-diary; /bin/launchctl unload ~/Library/LaunchAgents/org.mhjones.ical-to-diary.plist ; /bin/launchctl load ~/Library/LaunchAgents/org.mhjones.ical-to-diary.plist && /bin/launchctl start org.mhjones.ical-to-diary";
   };
 
+  # Snapshot org library once a minute
+  home.file."Library/LaunchAgents/org.mhjones.org-snapshot.plist" = {
+    text = ''
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>org.mhjones.org-snapshot</string>
+          <key>Program</key>
+          <string>${./macos/snapshot-org.sh}</string>
+          <key>StartInterval</key>
+          <integer>60</integer>
+          <key>WorkingDirectory</key>
+          <string>${homeDirectory}/org</string>
+        </dict>
+      </plist>
+    '';
+
+    onChange = "/bin/launchctl stop org.mhjones.org-snapshot; /bin/launchctl unload ~/Library/LaunchAgents/org.mhjones.org-snapshot.plist ; /bin/launchctl load ~/Library/LaunchAgents/org.mhjones.org-snapshot.plist && /bin/launchctl start org.mhjones.org-snapshot";
+  };
+
+
   home.file.".auto-dark-mode/auto-dark-mode.sh".source = ./macos/auto-dark-mode.sh;
   home.file.".auto-dark-mode/auto-dark-mode.swift".source = ./macos/auto-dark-mode.swift;
 
