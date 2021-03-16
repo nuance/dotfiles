71e46edbfff79886ed82b0bab1082d0380677bbf
Matthew Jones <matt@mhjones.org>
2021-03-16 11:47:04 -0400

    Install emacsclient / fd on remote linux hosts

--- home-manager/programs/setup-remote-host.nix
+++ home-manager/programs/setup-remote-host.nix
@@ -8,8 +8,12 @@
       scp -q ${./setup-remote-host/remote-profile} $hostname:.remote-profile
       scp -q ${./setup-remote-host/secrets/remote-editor} $hostname:.remote-editor
       scp -q ${./emacs/secrets/server} $hostname:.remote-emacs-server
+      ssh $hostname mkdir -p bin
+      scp -q ${./setup-remote-host/emacsclient.x86_64-linux} $hostname:bin/emacsclient
+      scp -q ${./setup-remote-host/fd.x86_64-linux} $hostname:bin/fd
+
       ssh $hostname "
-        [[ -e .bash_profile ]] || ( echo -n 'Creating .bash_profile'; cat > .bash_profile <<EOF && echo ok )
+        [[ -e .bash_profile ]] || ( echo -n 'Creating .bash_profile... '; cat > .bash_profile <<EOF && echo ok )
 # -*- mode: sh -*-
 
 # include .profile if it exists
@@ -20,8 +24,7 @@
 EOF
 "
       ssh $hostname "
-        echo -n 'Adding .remote-profile to .bash_profile... ';
-        [[ \$(grep REMOTE-EDITOR ~/.bash_profile) ]] || ( cat >> .bash_profile <<EOF && echo ok )
+        [[ \$(grep REMOTE-EDITOR ~/.bash_profile) ]] || ( echo -n 'Adding .remote-profile to .bash_profile... '; cat >> .bash_profile <<EOF && echo ok )
 # start REMOTE-EDITOR
 [[ -f ~/.remote-profile ]] && . ~/.remote-profile
 # end REMOTE-EDITOR
