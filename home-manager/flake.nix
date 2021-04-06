73ab5cddeefc46c9f84c145e351ee45229f6f7bd
Matthew Jones <matt@mhjones.org>
2021-04-06 09:12:52 -0400

    Bump emacs-overlay, using my fork

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -4,7 +4,7 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
   inputs.emacs-overlay = {
-    url = "github:nix-community/emacs-overlay";
+    url = "github:nuance/emacs-overlay-test-action";
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
