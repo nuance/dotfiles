9fda7cb04d279d7f1988226f427cacfa299664cf
Matthew Jones <matt@mhjones.org>
2021-04-21 15:43:49 -0400

    Re-enable ropen
    - Run the server locally via launchd
    - Tunnel a port for it
    - Install the client as part of setup-remote-host
    - Move ical-to-diary to a common utilities folder (for utility commands)

---
+++ utilities/ropen-client/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/gob"
+	"flag"
+	"io/ioutil"
+	"net"
+	"path/filepath"
+)
+
+var location = flag.String("location", "localhost:40001", "location to use")
+
+type Data struct {
+	Name     string
+	Contents []byte
+}
+
+func main() {
+	flag.Parse()
+
+	path := flag.Args()[0]
+	filename := filepath.Base(path)
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+
+	conn, err := net.Dial("tcp", *location)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	if err := gob.NewEncoder(conn).Encode(Data{Name: filename, Contents: data}); err != nil {
+		panic(err)
+	}
+}
