239d4be075053d0b4bc4806d7ecede7a4ec3c4af
Matthew Jones <matt@mhjones.org>
2021-05-03 11:03:04 -0400

    Pull stateVersions declarations up

--- home-manager/machines/dl.nix
+++ home-manager/machines/dl.nix
@@ -6,20 +6,12 @@
   homeDirectory = "/Users/matt";
   username = "matt";
 
+  stateVersion = "21.05";
+
   configuration = { pkgs, lib, config, ... }:
     {
       nixpkgs.overlays = overlays;
       programs.bash.shellAliases.flake-rebuild = "(cd ~/dotfiles/home-manager && ${../rebuild.sh} dl)";
       imports = [ ../config.nix ];
-
-      # This value determines the Home Manager release that your
-      # configuration is compatible with. This helps avoid breakage
-      # when a new Home Manager release introduces backwards
-      # incompatible changes.
-      #
-      # You can update Home Manager without changing this value. See
-      # the Home Manager release notes for a list of state version
-      # changes in each release.
-      home.stateVersion = "21.03";
     };
 }
--- home-manager/machines/work-pro.nix
+++ home-manager/machines/work-pro.nix
@@ -6,6 +6,8 @@
   homeDirectory = "/Users/matt";
   username = "matt";
 
+  stateVersion = "21.05";
+
   configuration = { pkgs, lib, config, ... }:
     {
       nixpkgs.overlays = overlays;
@@ -33,15 +35,5 @@
 
       programs.ssh.serverAliveCountMax = 1;
       programs.ssh.serverAliveInterval = 240;
-
-      # This value determines the Home Manager release that your
-      # configuration is compatible with. This helps avoid breakage
-      # when a new Home Manager release introduces backwards
-      # incompatible changes.
-      #
-      # You can update Home Manager without changing this value. See
-      # the Home Manager release notes for a list of state version
-      # changes in each release.
-      home.stateVersion = "21.03";
     };
 }
