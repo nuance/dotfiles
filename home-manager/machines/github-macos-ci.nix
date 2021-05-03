6d198c1a6d545dc81f774ff26e625178b1fd792b
Matthew Jones <matt@mhjones.org>
2021-05-03 11:03:04 -0400

    Pull stateVersions declarations up

--- home-manager/machines/github-macos-ci.nix
+++ home-manager/machines/github-macos-ci.nix
@@ -6,25 +6,12 @@
   homeDirectory = "/Users/runner";
   username = "runner";
 
+  stateVersion = "21.05";
+
   configuration = { pkgs, lib, config, ... }:
     {
       nixpkgs.overlays = overlays;
       programs.bash.shellAliases.flake-rebuild = "(cd ~/dotfiles/home-manager && ${../rebuild.sh} github-macos-ci)";
       imports = [ ../config.nix ../environments/macos.nix ];
-
-      # Home Manager needs a bit of information about you and the
-      # paths it should manage.
-      home.username = "runner";
-      home.homeDirectory = "/Users/runner";
-
-      # This value determines the Home Manager release that your
-      # configuration is compatible with. This helps avoid breakage
-      # when a new Home Manager release introduces backwards
-      # incompatible changes.
-      #
-      # You can update Home Manager without changing this value. See
-      # the Home Manager release notes for a list of state version
-      # changes in each release.
-      home.stateVersion = "21.03";
     };
 }
