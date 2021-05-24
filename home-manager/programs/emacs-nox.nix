0283faa8f80b7eb0871c97a2f7c520813889fc18
Matthew Jones <matt@mhjones.org>
2021-05-24 14:20:17 -0400

    simpler remote machine config

---
+++ home-manager/programs/emacs-nox.nix
@@ -0,0 +1,42 @@
+{ pkgs, lib, ... }:
+let
+  emacs = (pkgs.emacsWithPackagesFromUsePackage {
+    config = ./emacs/init.org;
+    alwaysEnsure = true;
+    alwaysTangle = true;
+    package = (pkgs.emacsGit-nox.override {
+      nativeComp = true;
+    });
+  });
+in
+{
+  home.packages = with pkgs;
+    [
+      emacs
+      gnupg1
+      ispell
+      nixpkgs-fmt
+      python37Packages.black
+      python37Packages.flake8
+      graphviz-nox
+      fd
+      ripgrep
+      pandoc
+    ];
+
+  programs.mu.enable = true;
+
+  home.file = {
+    ".emacs.d/init.org" = {
+      source = ./emacs/init.org;
+      onChange = "cd ~/.emacs.d ; ${emacs}/bin/emacs --batch -l ob-tangle --eval \"(org-babel-tangle-file \\\"init.org\\\")\"";
+    };
+    ".emacs.d/site-lisp/mu4e-thread-folding.el".source = ./emacs/mu4e-thread-folding.el;
+  };
+
+  programs.bash.sessionVariables.EDITOR = "${emacs}/bin/emacsclient -f $HOME/.emacs.d/server/server";
+  programs.zsh.sessionVariables.EDITOR = "${emacs}/bin/emacsclient";
+
+  programs.bash.shellAliases.en = "$EDITOR -n";
+  programs.zsh.shellAliases.en = "$EDITOR -n";
+}
