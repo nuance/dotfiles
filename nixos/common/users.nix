c709be6014efef90b016719079cd70e828934547
Matthew Jones <matt@mhjones.org>
2021-05-24 13:44:11 -0400

    consolidated top-level flake

--- nixos/common/users.nix
+++ nixos/common/users.nix
@@ -3,13 +3,10 @@ let keys = lib.splitString "\n" (builtins.readFile ./github-keys.txt);
 in
 {
   security.sudo.wheelNeedsPassword = false;
-  users.extraUsers.matt = {
-    createHome = true;
-    home = "/home/matt";
-    description = "Matt";
-    group = "users";
-    extraGroups = [ "wheel" ];
+  users.users.matt = {
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = keys;
+    extraGroups = [ "wheel" ];
+    isNormalUser = true;
   };
 }
