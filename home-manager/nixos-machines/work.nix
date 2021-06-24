b4ed87537bc5e2ba620a2c5934cbf579855c8941
Matthew Jones <matt@mhjones.org>
2021-06-24 10:52:45 -0400

    add nbdime / git-town aliases

--- home-manager/nixos-machines/work.nix
+++ home-manager/nixos-machines/work.nix
@@ -4,5 +4,30 @@
 
   home.packages = with pkgs; [
     git-town
+    python3Packages.nbdime
+  ];
+
+  programs.git.aliases = {
+    pychanged = "! git changed | egrep '.py$'";
+    fix = "! git pychanged | xargs black && git pychanged | xargs isort";
+    lint = "! git pychanged | xargs flake8 && git pychanged | xargs pyright";
+    test = "! git pychanged | xargs dirname | sort | uniq | xargs pytest";
+    flt = "! git fix && git lint && git test";
+    append = "town append";
+    hack = "town hack";
+    kill = "town kill";
+    new-pull-request = "town new-pull-request";
+    prepend = "town prepend";
+    prune-branches = "town prune-branches";
+    rename-branch = "town rename-branch";
+    repo = "town repo";
+    ship = "town ship";
+    sync = "town sync";
+  };
+
+  programs.git.includes = [
+    {
+      "path" = "${./nbdime.inc}";
+    }
   ];
 }
