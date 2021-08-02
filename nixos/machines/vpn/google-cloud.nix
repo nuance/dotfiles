8f7783af2e8c931d1caedee623fb3fffa8f28d32
Matthew Jones <matt@mhjones.org>
2021-08-02 12:00:49 -0400

    don't explicitly install the sdk

--- nixos/machines/vpn/google-cloud.nix
+++ nixos/machines/vpn/google-cloud.nix
@@ -5,9 +5,6 @@
     (modulesPath + "/virtualisation/google-compute-image.nix")
   ];
 
-  environment.systemPackages = with pkgs; [
-    google-cloud-sdk-gce
-  ];
   boot.kernelParams = [
     "console=tty1"
     "console=ttyS0,115200"
